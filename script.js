4efdf3fe02d8fcecdab6dfd0089d27aa18db07bb
”fullstackmalina” <malinkin@mail.ru>
2023-03-13 18:55:38 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+"use strict";
+
+// const hamburger = 5;
+// const fries = null;
+// const cola = 1;
+
+// if (hamburger && fries) {
+//     console.log("я сыт");
+// }
+
+// const hamburger = 3;
+// const fries = 1;
+// const cola = 0;
+
+// console.log(hamburger === 3 && cola && fries)
+
+// if (hamburger === 3 && cola === 1 && fries) {
+//     console.log("все сыты");
+// } else {
+//     console.log("Мы уходим");
+// }
+
+
+const hamburger = 3;
+const fries = 0;
+const cola = 0;
+
+if (hamburger || cola || fries) {
+    console.log("Все довольны");
+} else {
+    console.log("Мы уходим");
+}
